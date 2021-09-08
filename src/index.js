fce70fa18758b1f5aba59ca863c5aa6096467ec0
Davage-M <davagemd@gmail.com>
2021-09-07 22:15:32 -0400

    adding files

--- src/index.js
+++ src/index.js
@@ -36,6 +36,7 @@ async function main(twitterHandle, pagination) {
 
 app.get('/search_tweets', async (req, res) => {
     //console.log("Loading Data........");
+    //res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.set('Access-Control-Allow-Origin', 'https://davage-m.github.io');
     let requestQuery = req.query;
     let pagination = parseInt(requestQuery.pagination, 10);
